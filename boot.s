85d77ed5b53a1bd5642985ca7c6e563c58c98fa1
Duje Mihanović <duje.mihanovic@skole.hr>
2021-08-21 17:06:36 +0200

    Allow controlling (no) newline when printing and listen to keystrokes

--- boot.s
+++ boot.s
@@ -1,21 +1,69 @@
 	bits 16 ; boot sectors run in real mode
 	org 7c00h ; boot sector is loaded at 0x7c00
 
+_start
 	mov di, printing_numbers
 	call print
 
+	or bl, 1
 	mov dx, 0DEADh ; number to print
 	call print_word
+	
+	mov di, space
+	call print
+	
+	xor bl, bl
 	mov dx, 0BEEFh
 	call print_word
 
-	mov di, goodbye
+	mov di, waiting
 	call print
 
-	jmp $
+	mov di, keystroke
+	mov bl, 1
 
-printing_numbers: db "Printing some random hex numbers", 0
-goodbye: db "Thank you for your attention", 0
+.loop
+	mov ah, 0
+	int 16h
+	cmp ah, 1Ch
+	je .enterpressed
+	jmp .continue
+	cmp ah, 0Eh
+	je .backspace
+	jmp .continue
+.enterpressed
+	pusha
+	mov ah, 0Eh
+	mov al, 0Dh
+	int 10h
+	mov al, 0Ah
+	int 10h
+	popa
+	jmp .loop
+.backspace
+	pusha
+	mov ah, 03h
+	mov bh, 0
+	int 10h
+	dec dl
+	mov ah, 02h
+	int 10h
+	mov ah, 0Eh
+	mov al, ' '
+	int 10h
+	mov ah, 02h
+	int 10h
+	popa
+	jmp .loop
+.continue
+	mov [keystroke], al
+	call print
+	jmp .loop
+
+printing_numbers db "Printing some random hex numbers", 0
+waiting db "I await keystrokes", 0
+keystroke db "$", 0
+space db " ", 0
 
 %include "print.s"
 
