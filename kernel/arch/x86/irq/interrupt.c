b87962874082db4093c960cf613eedae918180fc
Duje Mihanović <duje.mihanovic@skole.hr>
2022-06-14 11:23:08 +0200

    Use interrupt frame and new double fault handler

--- kernel/arch/x86/irq/interrupt.c
+++ kernel/arch/x86/irq/interrupt.c
@@ -5,16 +5,17 @@
 #include <irq/i8259a.h>
 
 /* This table will hold pointers to our interrupt handlers. */
-static int (*int_handler_table[256])(void);
+static int (*int_handler_table[256])(struct interrupt_frame *);
 
-void int_handler(int interrupt)
+void int_handler(struct interrupt_frame *frame)
 {
+	int interrupt = frame->interrupt;
 	if (int_handler_table[interrupt] == NULL) {
 		kprint("WARNING: Unhandled interrupt ", 0);
 		kprintb(interrupt);
 		kprint(" occurred!\n", 0);
 	} else {
-		int ret = (*int_handler_table[interrupt])();
+		int ret = (*int_handler_table[interrupt])(frame);
 		if (ret) {
 			kprint("WARNING: Error while handling interrupt ", 0);
 			kprintb(interrupt);
@@ -26,7 +27,7 @@ void int_handler(int interrupt)
 	}
 }
 
-int register_interrupt(int irq, int (*handler)(void))
+int register_interrupt(int irq, int (*handler)(struct interrupt_frame *))
 {
 	int_handler_table[irq] = handler;
 	if (irq >= 32 && irq < 48) {
